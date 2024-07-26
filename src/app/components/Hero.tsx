4a96682ef17f5e2c3af37f67fe7d4ca56a4b207d
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2024-07-26 20:27:59 +0700

    Finished Hero text section
    - Installed ShadCN along with configuring it's components.json
    - (WARNING!) utils.ts might be changed
    - (WARNING!) globals.css might be changed
    - (WARNING!) tailwind.config.ts was overwritten by ShadCN. Problem was fixed and both before and after files have been merged by ChatGPT. Just watch out this might be a cause of problem in the future
    - Added none variant to ShadCN's button component so I can write my own styles without tampering with ShadCN's default styles

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,6 +4,31 @@ import { HeroHighlight } from "./ui/hero-highlight";
 import Image from "next/image";
 import profileImage from "public/images/bw-cool.png";
 import { FlipWords } from "./ui/flip-words";
+import { Button } from "@/components/ui/button";
+
+// Define the variants
+const containerVariants = {
+  visible: {
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: [20, -5, 0],
+    transition: {
+      duration: 0.5,
+      ease: [0.4, 0.0, 0.2, 1],
+    },
+  },
+};
 
 export default function Hero() {
   const words = [
@@ -22,35 +47,52 @@ export default function Hero() {
   ];
 
   return (
-    // <div className="w-screen">
-    <HeroHighlight className="flex items-center justify-center p-[10vw]">
-      <div className="hi there">
+    <HeroHighlight className="flex max-w-[85vw] items-center justify-center">
+      {/* TEXT */}
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+        className="flex flex-col items-start justify-center gap-12"
+      >
         <motion.h1
-          initial={{
-            opacity: 0,
-            y: 20,
-          }}
-          animate={{
-            opacity: 1,
-            y: [20, -5, 0],
-          }}
-          transition={{
-            duration: 0.5,
-            ease: [0.4, 0.0, 0.2, 1],
-          }}
-          className="mx-auto max-w-4xl px-4 text-center text-2xl font-bold leading-relaxed text-neutral-700 dark:text-white md:text-3xl lg:text-4xl lg:leading-snug"
+          variants={childVariants}
+          className="text-2xl font-bold leading-relaxed text-neutral-700 dark:text-white md:text-4xl lg:text-5xl lg:leading-snug"
         >
           Hi, I'm <FlipWords words={words} />
         </motion.h1>
-      </div>
-      {/* <h1 className="bg-gradient-text text-transparent bg-clip-text">
-        This is a test
-      </h1> */}
-      {/* <div className="blob relative max-w-[40%] max-h-[40%] overflow-hidden  border-red-500">
-        <div className="magicpattern"></div>
+
+        <motion.p
+          variants={childVariants}
+          className="text-lg text-neutral-600 dark:text-neutral-400 md:text-xl"
+        >
+          "A software engineer specializing on Front-End Web Development, who
+          loves to bridge the gap between beautiful design and seamless
+          functionality to create exceptional user experiences"
+        </motion.p>
+
+        <div className="group/hero_cta relative z-0 ml-2 w-fit">
+          <div className="absolute -z-10 h-full w-full rounded-full bg-gradient-to-r from-[#5060F0] to-[#6E7AEA] opacity-0 transition-all duration-200 group-hover/hero_cta:opacity-100"></div>
+          <Button
+            variant="none"
+            size="lg"
+            className="text-porto_purple border-porto_purple rounded-full border-2 bg-transparent px-5 py-5 font-semibold tracking-wide transition-all duration-200 group-hover/hero_cta:text-white md:text-xl"
+          >
+            Contact Me
+          </Button>
+        </div>
+
+        {/* <motion.button
+          className="text-porto_purple border-porto_purple ml-2 rounded-full border-2 bg-transparent bg-gradient-to-r px-5 py-3 font-semibold tracking-wide transition-all duration-200 hover:from-[#5060F0] hover:to-[#6E7AEA] hover:text-white md:text-xl"
+        >
+          Contact Me
+        </motion.button> */}
+      </motion.div>
+
+      {/* IMAGE */}
+      <div>
         <Image src={profileImage} alt="Profile Image"></Image>
-      </div> */}
+      </div>
     </HeroHighlight>
-    // </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,34 +5,109 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  darkMode: "class",
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      colors: {
+        porto_purple: "#5060F0",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "gradient-text": "linear-gradient(90deg, #816ED8 0%, #7568DD 19%, #565ED3 38%, #559CDF 62%, #6FC0AC 81%, #70CEA8 100%)",
+        "gradient-text":
+          "linear-gradient(90deg, #816ED8 0%, #7568DD 19%, #565ED3 38%, #559CDF 62%, #6FC0AC 81%, #70CEA8 100%)",
+      },
+      fontFamily: {
+        "plus-jakarta-sans": ["Plus Jakarta Sans", "sans-serif"],
       },
     },
   },
   plugins: [
+    require("tailwindcss-animate"),
     addVariablesForColors,
     function ({ matchUtilities, theme }: any) {
       matchUtilities(
         {
           "bg-dot-thick": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18" fill="none"><circle fill="${value}" id="pattern-circle" cx="16" cy="16" r="3"></circle></svg>`
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="18" height="18" fill="none"><circle fill="${value}" id="pattern-circle" cx="16" cy="16" r="3"></circle></svg>`,
             )}")`,
           }),
         },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: "color",
+        },
       );
     },
   ],
@@ -42,11 +117,12 @@ const config: Config = {
 function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
   );
 
   addBase({
     ":root": newVars,
   });
 }
+
 export default config;
