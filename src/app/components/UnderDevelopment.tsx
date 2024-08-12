109be75bc9ba9eac1e9bfe3a21d6415b7125c86c
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2024-08-12 23:00:46 +0700

    Added UnderDevelopment notice + activated <Contact/> bg animation
    - Switched mt from <Contact/> to mb in <About/>

---
+++ src/app/components/UnderDevelopment.tsx
@@ -0,0 +1,11 @@
+export default function UnderDevelopment() {
+  return (
+    <div className="relative grid h-[75vh] place-items-center px-[10vw] md:px-[20vw]">
+      <h1 className="text-center text-3xl font-medium sm:text-4xl">
+        This page is still under development. Please stay tuned for the complete
+        version :D
+      </h1>
+      <div className="box-shadow-fade absolute inset-0"></div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import UnderDevelopment from "./components/UnderDevelopment";
 
 export default function Home() {
   return (
   <>
-    <Hero /> 
+    <Hero />
     <About />
+    <UnderDevelopment />
     <Contact />
     <Footer />
   </>
