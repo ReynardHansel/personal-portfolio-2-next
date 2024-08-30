a35b86eaa23476a07f3b0739d59582ec117774d2
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2024-08-30 17:05:40 +0700

    Hide Navbar on scroll and moved <Navbar/> from <Hero/> to layout.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,9 +7,6 @@ import Navbar from "./Navbar";
 export default function Hero() {
   return (
     <HeroHighlight className="flex flex-col max-w-[80vw] items-center justify-center">
-      {/* NAVBAR */}
-      <Navbar />
-
       <div className="flex">
         {/* TEXT */}
         <HeroText />
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,15 +1,60 @@
+"use client";
+
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import Link from "next/link";
+import { useState } from "react";
+
+const NavbarVariants = {
+  hidden: {
+    y: -100,
+  },
+  visible: {
+    y: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.2,
+    },
+  },
+};
 
 export default function Navbar() {
+  const { scrollY } = useScroll();
+  const [hidden, setHidden] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious()!;
+    if (latest > previous && latest > 150) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+    // console.log(latest, previous);
+  });
+
   return (
-    <div className="fixed top-0 flex justify-between items-center h-[13vh] z-10 w-full px-[10vw]">
-        <h2 className="font-bold font-nunito-sans text-lg sm:text-2xl">Reynard H.</h2>
-        <div className="flex gap-[6vw] sm:text-lg justify-between font-nunito-sans">
-            <Link href="#about" className="">About</Link>
-            <Link href="#skillset" className="">Skill Set</Link>
-            <Link href="#projects" className="">Projects</Link>
-            <Link href="#contact" className="">Contact</Link>
-        </div>
-    </div>
-  )
+    <motion.nav
+      variants={NavbarVariants}
+      initial="hidden"
+      animate={hidden ? "hidden" : "visible"}
+      className="fixed top-0 z-50 flex h-[13vh] w-full items-center justify-between px-[10vw]"
+    >
+      <h2 className="font-nunito-sans text-lg font-bold sm:text-2xl">
+        Reynard H.
+      </h2>
+      <div className="flex justify-between gap-[6vw] font-nunito-sans sm:text-lg">
+        <Link href="#about" className="">
+          About
+        </Link>
+        <Link href="#skillset" className="">
+          Skill Set
+        </Link>
+        <Link href="#projects" className="">
+          Projects
+        </Link>
+        <Link href="#contact" className="">
+          Contact
+        </Link>
+      </div>
+    </motion.nav>
+  );
 }
