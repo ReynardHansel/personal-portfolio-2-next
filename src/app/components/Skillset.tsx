7ff039a85583dd57528227e3da5a65175b016d58
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2026-01-25 21:17:49 +0700

    change(skillset): Modified the skillset section text

--- src/app/components/Skillset.tsx
+++ src/app/components/Skillset.tsx
@@ -76,9 +76,12 @@ export default function Skillset() {
   return (
     <div
       id="skillset"
-      className="my-[15vh] flex flex-col items-center justify-center gap-[6vh] px-4"
+      className="my-[15vh] flex flex-col items-center justify-center gap-[5vh] px-4"
     >
       <Title>Skill Set</Title>
+      <p className="px-[10vw] text-center font-plus-jakarta-sans tracking-wider md:px-[15vw] lg:px-[15vw] lg:text-lg">
+      Constantly experimenting and evolving. This is the tech stack I currently work with, while I keep learning and adding new tools to my belt.
+      </p>
       <motion.div
         ref={containerRef}
         className="flex max-w-5xl flex-wrap gap-4 px-[18%] sm:px-28"
