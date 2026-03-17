29f32161d400494a0ce2af2f4367a8fd3ebe8b6c
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2026-03-18 00:04:19 +0700

    feat(blob): Added blob back to skillset

--- src/app/components/Skillset.tsx
+++ src/app/components/Skillset.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
 import Title from "./Reusable/Title";
 import SkillContainer from "./Reusable/SkillContainer";
 import { skillData } from "./Skillset/skillData";
+
+import blob2 from "public/svg/blobs/about-right.svg";
 
 export default function Skillset() {
   return (
@@ -8,6 +11,15 @@ export default function Skillset() {
       id="skillset"
       className="my-[15vh] flex flex-col items-center justify-center gap-[5vh] px-4"
     >
+      {/* BLOB */}
+      <div className="absolute -bottom-32 right-0 z-0 overflow-visible md:-bottom-[37vh]">
+        <Image
+          src={blob2}
+          alt=""
+          className="relative -right-[45vw] h-[20vh] sm:-right-80 md:-right-96 md:h-[30vh] lg:h-[50vh]"
+        />
+      </div>
+
       <Title>Skill Set</Title>
       <p className="px-[10vw] text-center font-plus-jakarta-sans tracking-wider md:px-[15vw] lg:px-[15vw] lg:text-lg">
         Constantly experimenting and evolving. This is the tech stack I
