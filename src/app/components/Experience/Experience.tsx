7c6503d30d3321536a63ac19a7c08c5402f8e0ed
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2026-03-21 20:31:19 +0700

    feat(exp): seperated Experience and Details section in <Experience />

--- src/app/components/Experience/Experience.tsx
+++ src/app/components/Experience/Experience.tsx
@@ -4,31 +4,48 @@ export default function Experience() {
   return (
     <section className="flex w-full flex-col items-center justify-center gap-[5vh]">
       <Title>Experience</Title>
-      <div className="space-around gap-5 flex h-full min-h-[80vh] w-full flex-col items-center justify-center px-6 py-8 md:flex-row md:items-start md:py-0 md:px-[5%]">
+      <div className="space-around flex h-full min-h-[80vh] w-full flex-col items-center justify-center gap-5 px-6 py-8 md:flex-row md:items-start md:px-[5%] md:py-0">
         {/* Experience */}
-        <div className="flex flex-1 flex-col items-start justify-center">
-          <div className="flex items-center gap-3 text-sm font-semibold uppercase tracking-widest text-neutral-500">
-            {/* <span className="h-2.5 w-2.5 rounded-full bg-neutral-600"></span> */}
-            2023 — PRESENT
-          </div>
-
-          <p className="mt-10 text-[2.5rem] font-bold uppercase leading-none tracking-tight text-[#2d3334] sm:text-[3.25rem]">
-            METAVERS LABS
-          </p>
-
-          <p className="mt-2 text-[1.3rem] font-light text-[#5a5f60]">
-            Lead Design Systems Architect
-          </p>
-        </div>
+        <ExperienceSection />
 
         {/* Divider Line */}
         <div className="hidden w-0.5 self-stretch bg-black md:block"></div>
 
         {/* Details */}
         <div className="border-blue-5 flex flex-[2] flex-col items-center justify-center border-2 py-8 md:py-0">
-          <h1>Details</h1>
+          <DetailsSection />
         </div>
       </div>
     </section>
   );
+}
+
+function ExperienceSection() {
+  return (
+    <div className="flex flex-1 flex-col items-start justify-center">
+      {/* Duration */}
+      <div className="flex items-center gap-3 text-sm font-semibold uppercase tracking-widest text-neutral-500">
+        {/* <span className="h-2.5 w-2.5 rounded-full bg-neutral-600"></span> */}
+        2023 — PRESENT
+      </div>
+
+      {/* Company */}
+      <p className="mt-10 text-[2.5rem] font-bold uppercase leading-none tracking-tight text-[#2d3334] sm:text-[3.25rem]">
+        METAVERS LABS
+      </p>
+
+      {/* Job Title */}
+      <p className="mt-2 text-[1.3rem] font-light text-[#5a5f60]">
+        Lead Design Systems Architect
+      </p>
+    </div>
+  );
+}
+
+function DetailsSection(){
+  return(
+    <div>
+      
+    </div>
+  )
 }
