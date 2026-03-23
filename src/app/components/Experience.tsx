48d514c3d9ce1cd1c1bdc2f26500be012ded8338
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2026-03-24 02:16:29 +0700

    feat(exp): ExperienceSection animation done

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 import Title from "./Reusable/Title";
 import SkillCard from "./Skillset/SkillCard";
 import Image from "next/image";
+import { animate, motion, Variants } from "framer-motion";
 
 import {
   Carousel,
@@ -11,15 +12,27 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
-
 import Fade from "embla-carousel-fade";
 import Autoplay from "embla-carousel-autoplay";
+
+// const containerVariants = {
+//   stagger: {
+//     transition: {
+//       staggerChildren: 0.7,
+//     },
+//   },
+// };
 
 export default function Experience() {
   return (
     <section className="flex w-full flex-col items-center justify-center gap-[5vh]">
       <Title>Experience</Title>
-      <div className="space-around flex h-full min-h-[80vh] w-full flex-col items-center justify-center gap-[5vw] px-6 py-8 md:flex-row md:items-start md:px-[5%] md:py-0">
+      <motion.div
+        className="space-around flex h-full min-h-[80vh] w-full flex-col items-center justify-center gap-[5vw] px-6 py-8 md:flex-row md:items-start md:px-[5%] md:py-0"
+        initial="initial"
+        animate="animate"
+        whileInView="whileInView"
+      >
         {/* Experience */}
         <ExperienceSection />
 
@@ -28,35 +41,69 @@ export default function Experience() {
 
         {/* Details */}
         <DetailsSection />
-      </div>
+      </motion.div>
     </section>
   );
 }
 
 function ExperienceSection() {
+  const containerVariants: Variants = {
+    initial: { opacity: 0 },
+    whileInView: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const itemVariants: Variants = {
+    initial: { opacity: 0, x: -50 },
+    whileInView: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 1.5,
+        ease: [0.16, 1, 0.3, 1],
+      },
+    },
+  };
+
   return (
-    <div className="flex flex-1 flex-col items-start justify-center md:py-10">
+    <motion.div
+      className="flex flex-1 flex-col items-start justify-center md:py-10"
+      variants={containerVariants}
+    >
       {/* Duration */}
-      <div className="flex items-center gap-3 text-sm font-semibold uppercase tracking-widest text-neutral-500">
+      <motion.div 
+        variants={itemVariants}
+        className="flex items-center gap-3 text-sm font-semibold uppercase tracking-widest text-neutral-500"
+      >
         {/* <span className="h-2.5 w-2.5 rounded-full bg-neutral-600"></span> */}
         2023 — PRESENT
-      </div>
+      </motion.div>
 
       {/* Company */}
-      <p className="mt-10 text-[2.5rem] font-bold uppercase leading-none tracking-tight text-[#2d3334] sm:text-[3.25rem]">
+      <motion.p 
+        variants={itemVariants}
+        className="mt-10 text-[2.5rem] font-bold uppercase leading-none tracking-tight text-[#2d3334] sm:text-[3.25rem]"
+      >
         Apple Developer Academy
-      </p>
+      </motion.p>
 
       {/* Job Title */}
-      <p className="mt-2 text-[1.3rem] font-light text-[#5a5f60]">
+      <motion.p 
+        variants={itemVariants}
+        className="mt-2 text-[1.3rem] font-light text-[#5a5f60]"
+      >
         Junior iOS Developer
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 }
 
 function DetailsSection() {
-  const [api, setApi] = useState<CarouselApi>();
+  // const [api, setApi] = useState<CarouselApi>();
 
   return (
     <div className="flex flex-[2] flex-col items-start justify-between gap-[4vh] self-stretch pt-8 md:pt-6">
@@ -97,6 +144,7 @@ function DetailsSection() {
       {/* <div className="aspect-[16/10] w-[95%] rounded-md border border-neutral-300 bg-gradient-to-br from-neutral-200 to-neutral-400"></div> */}
       <Carousel
         className="w-[95%]"
+        // setApi={setApi}
         opts={{
           align: "start",
           loop: true,
@@ -147,4 +195,3 @@ function DetailsSection() {
     </div>
   );
 }
-
